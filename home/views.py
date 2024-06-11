6c0c3769bd32bd826a0eed4d1976218bdfd3d6d2
Phil Tipping <philtipping3110@gmail.com>
2024-06-11 12:05:17 +0000

    Link base.html to index.html

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
 
-def lego_blog(request):
-    return HttpResponse("Lets share Lego!")
+class Home(TemplateView):
+    """
+    Class based view to render home page.
+    """
+    template_name = 'home/index.html'
+
