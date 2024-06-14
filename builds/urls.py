204520144cf73576a700789426dc70a45b43a1dc
Phil Tipping <philtipping3110@gmail.com>
2024-06-14 15:21:01 +0000

    Add delete view and functionality

--- builds/urls.py
+++ builds/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import AddBuild, Builds
+from .views import AddBuild, Builds, DeleteBuild
 from . import views
 
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path("add/", AddBuild.as_view(), name="add_build"),
     path('', Builds.as_view(), name="builds"),
     path('<slug:slug>/', views.build_view, name='build_view'),
+    path('delete/<slug:pk>/', DeleteBuild.as_view(), name='delete_build'),
 
 ]
--- builds/views.py
+++ builds/views.py
@@ -1,10 +1,11 @@
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, DeleteView
 from django.views import generic
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
-from django.contrib.auth.mixins import LoginRequiredMixin
-
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, UserPassesTestMixin
+) 
 from .models import Build
 from .forms import BuildForm
 
@@ -55,5 +56,23 @@ class AddBuild(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         response = super().form_valid(form)
-        messages.add_message(self.request, messages.SUCCESS, f'{self.request.user} your post was submitted')
+        messages.add_message(self.request, messages.SUCCESS, f'{self.request.user} your build post was succesfully submitted')
         return response
+
+
+class DeleteBuild(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    Delete a lego build post.
+    """
+    model = Build
+    success_url = "/builds/"
+
+    def test_func(self):
+        return self.request.user == self.get_object().user
+
+    def delete(self, request, *args, **kwargs):
+        response = super().delete(request, *args, **kwargs)
+        messages.add_message(self.request, messages.SUCCESS, f'{self.request.user} your build post was succesfully deleted')
+        return response
+
+
