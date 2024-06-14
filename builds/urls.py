ad03e0c1b3cd0de703d3313b5d76891bad4ef807
Phil Tipping <philtipping3110@gmail.com>
2024-06-14 11:57:46 +0000

    Finish single build view and add success message for post

--- builds/urls.py
+++ builds/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import AddBuild, Builds
+from . import views
 
 
 urlpatterns = [
     path("add/", AddBuild.as_view(), name="add_build"),
-    path('', Builds.as_view(), name="builds")
+    path('', Builds.as_view(), name="builds"),
+    path('<slug:slug>/', views.build_view, name='build_view'),
+
 ]
--- builds/views.py
+++ builds/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import CreateView, ListView
 from django.views import generic
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -7,6 +9,29 @@ from .models import Build
 from .forms import BuildForm
 
 # Create your views here.
+
+def build_view(request, slug):
+    """
+    Display an individual :model:`build.Build`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`build.Build`.
+
+    **Template:**
+
+    :template:`builds/build_view.html`
+    """
+
+    queryset = Build.objects.all()
+    build = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "builds/build_view.html",
+        {"build": build },
+    )
 
 class Builds(generic.ListView):
     """
@@ -20,7 +45,7 @@ class Builds(generic.ListView):
 
 class AddBuild(LoginRequiredMixin, CreateView):
     """ 
-    A class view to add a lego build.
+    A class view to add a Lego build.
     """
     template_name = 'builds/add_build.html'
     model = Build
@@ -29,4 +54,6 @@ class AddBuild(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(AddBuild, self).form_valid(form)
+        response = super().form_valid(form)
+        messages.add_message(self.request, messages.SUCCESS, f'{self.request.user} your post was submitted')
+        return response
